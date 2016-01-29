d62681ab727ed002c42338668900dd487b492c77
dipuhasan <depu1994@gmail.com>
2016-01-29 13:21:03 +0600

    Create decimanTohex.cpp

---
+++ Binary_Decimal/decimanTohex.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long int decimal,rem,quotient;
+    int i=1,j,temp;
+    char hex[1001];
+    scanf("%ld",&decimal);
+    quotient = decimal;
+    while(quotient != 0){
+         temp = quotient % 16;
+      //To convert integer into character
+      if( temp < 10)
+           temp = temp + 48;
+      else
+         temp = temp + 55;
+
+      hex[i++]= temp;
+      quotient = quotient / 16;
+  }
+
+    for(j = i -1; j>0; j--){
+            printf("%c", hex[j]);
+    }
+
+    return 0;
+}
+
+Input : 10 15 16 31 65535
+Output: A  F  10 1F FFFF
